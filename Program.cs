3b59440cc5c008e0f1ae870dc964e6588ecc7473
leaghado <80654971+leaghado@users.noreply.github.com>
2021-10-24 18:12:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var driver = new ChromeDriver())
+            {
+                
+                driver.Manage().Window.Maximize();
+                
+                //Переменная для задержки
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                
+                //Переходим на ссылку установки расширения метамаска
+                driver.Navigate().GoToUrl(@"https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=ru");
+
+                var x0 = driver.CurrentWindowHandle;
+                //Получаем дескриптор окна браузера
+                var mainWindow = WinApi.GetForegroundWindow();
+                
+                //Нажимаем кнопку установить(Metamask)
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[@class='g-c-R  webstore-test-button-label']"))).Click();
+                Thread.Sleep(3000);
+
+                //Получаем дескриптор окна
+                var modalWindow = WinApi.GetForegroundWindow();
+                //WinApi.MoveWindow(modalWindow, 0, 0, 400, 300, false);
+                
+                //Поднимаем наверх модальное окно
+                WinApi.SetWindowPos(modalWindow, (IntPtr)0, 0, 0, 0, 0, SetWindowPosFlags.NOMOVE | SetWindowPosFlags.NOSIZE | SetWindowPosFlags.NOZORDER | SetWindowPosFlags.SHOWWINDOW);
+                
+                //Задаем позицию курсора
+                WinApi.SetCursorPos(1325, 275);
+
+                //Нажимаешь левую кнопку мыши
+                WinApi.MouseClickLeft();
+                
+                //Поднимаем вверх окно браузера
+                WinApi.SetWindowPos(mainWindow, (IntPtr)0, 0, 0, 0, 0, SetWindowPosFlags.NOMOVE | SetWindowPosFlags.NOSIZE | SetWindowPosFlags.NOZORDER | SetWindowPosFlags.SHOWWINDOW);
+                Thread.Sleep(5000);
+                var x2 = driver.WindowHandles;
+                driver.SwitchTo().Window(x2[1]);
+                //
+                wait.Until(e => e.FindElement(By.XPath("//button"))).Click();
+                wait.Until(e => e.FindElement(By.CssSelector("#app-content > div > div.main-container-wrapper > div > div > div.select-action__wrapper > div > div.select-action__select-buttons > div:nth-child(2) > button"))).Click();
+                wait.Until(e => e.FindElement(By.XPath("//button[@class ='button btn-primary page-container__footer-button']"))).Click();
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("create-password")).SendKeys("fccjkm0107");
+                driver.FindElement(By.Id("confirm-password")).SendKeys("fccjkm0107");
+                wait.Until(e => e.FindElement(By.XPath("//div[@class ='first-time-flow__checkbox']"))).Click();
+                wait.Until(e => e.FindElement(By.XPath("//button[@class ='button btn-primary first-time-flow__button']"))).Click();
+                try
+                {
+                    var x4 = driver.FindElement(By.XPath("//button"));
+                    x4.Click();
+                }
+                catch
+                {
+                    var x4 = driver.FindElement(By.XPath("//button"));
+                    x4.Click();
+                }
+                wait.Until(e => e.FindElement(By.CssSelector("#app-content > div > div.main-container-wrapper > div > div > div.reveal-seed-phrase > div.reveal-seed-phrase__buttons > button.button.btn-secondary.first-time-flow__button"))).Click();
+                wait.Until(e => e.FindElement(By.XPath("//button[@class ='fas fa-times popover-header__button'"))).Click();
+                wait.Until(e => e.FindElement(By.XPath("//div[@class ='identicon'"))).Click();
+                wait.Until(e => e.FindElement(By.XPath("//div[@class ='account - menu__item__text'"))).Click();
+
+
+                driver.Quit();
+            }   
+        }
+    }
+}
---
+++ WinApi.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    [Flags]
+    public enum SetWindowPosFlags
+    {
+        NOSIZE = 0x0001,
+        NOMOVE = 0x0002,
+        NOZORDER = 0x0004,
+        NOREDRAW = 0x0008,
+        NOACTIVATE = 0x0010,
+        DRAWFRAME = 0x0020,
+        FRAMECHANGED = 0x0020,
+        SHOWWINDOW = 0x0040,
+        HIDEWINDOW = 0x0080,
+        NOCOPYBITS = 0x0100,
+        NOOWNERZORDER = 0x0200,
+        NOREPOSITION = 0x0200,
+        NOSENDCHANGING = 0x0400,
+        DEFERERASE = 0x2000,
+        ASYNCWINDOWPOS = 0x4000,
+    }
+    public class WinApi
+    {
+        public const uint MOUSEEVENTF_ABSOLUTE = 0x8000;
+        public const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        public const uint MOUSEEVENTF_LEFTUP = 0x0004;
+        public const uint MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        public const uint MOUSEEVENTF_MIDDLEUP = 0x0040;
+        public const uint MOUSEEVENTF_MOVE = 0x0001;
+        public const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        public const uint MOUSEEVENTF_RIGHTUP = 0x0010;
+        public const uint MOUSEEVENTF_XDOWN = 0x0080;
+        public const uint MOUSEEVENTF_XUP = 0x0100;
+        public const uint MOUSEEVENTF_WHEEL = 0x0800;
+        public const uint MOUSEEVENTF_HWHEEL = 0x01000;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, SetWindowPosFlags uFlags);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+        
+        public static void MouseClickLeft()
+        {
+            mouse_event(WinApi.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(WinApi.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+    }
+
+}
